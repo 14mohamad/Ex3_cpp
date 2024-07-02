701ace507d02f103928d44e3409106bc0b4516f8
Mohamad Mousa <93079380+14mohamad@users.noreply.github.com>
2024-07-02 22:10:07 +0300

    Add files via upload

---
+++ test/TestBoard.cpp
@@ -0,0 +1,11 @@
+#include <cassert>
+#include <iostream>
+#include "Board.h"
+#include "Tile.h"
+
+void testBoard() {
+    Board board;
+    assert(board.tiles.size() == 19);
+    assert(board.tiles[0].resourceType == ResourceType::WOOD);
+    assert(board.tiles[0].number == 5);
+}
---
+++ test/TestTile.cpp
@@ -0,0 +1,10 @@
+#include <cassert>
+#include <iostream>
+#include "Tile.h"
+#include "ResourceType.h"
+
+void testTile() {
+    Tile tile(ResourceType::WOOD, 5);
+    assert(tile.resourceType == ResourceType::WOOD);
+    assert(tile.number == 5);
+}
